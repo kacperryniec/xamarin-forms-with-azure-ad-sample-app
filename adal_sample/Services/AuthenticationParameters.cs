fb9fd3a791561a7b01a6925510c02ae6d195c8f4
Kacper Ryniec <kacper.ryniec@solidbrain.com>
2016-11-01 20:21:06 +0100

    Added login button with wired up OnClick handler

---
+++ adal_sample/Services/AuthenticationParameters.cs
@@ -0,0 +1,10 @@
+namespace adal_sample
+{
+	public static class AuthenticationParameters
+	{
+		public const string Authority = "https://login.windows.net/common";
+		public const string GraphResourceUri = "https://graph.windows.net";
+		public const string ApplicationId = "your-application-id";
+		public const string ReturnUri = "http://your-return-uri.com/";
+	}
+}
--- adal_sample/adal_samplePage.xaml.cs
+++ adal_sample/adal_samplePage.xaml.cs
@@ -7,6 +7,30 @@ namespace adal_sample
 		public adal_samplePage()
 		{
 			InitializeComponent();
+			logMeInBtn.Clicked += LogMeInBtn_Clicked;
+		}
+
+		private async void LogMeInBtn_Clicked(object sender, System.EventArgs e)
+		{
+			var authenticationService = DependencyService.Get<IAuthenticationService>();
+
+			try
+			{
+				var authenticationResult = await authenticationService.Authenticate(
+					AuthenticationParameters.Authority,
+					AuthenticationParameters.GraphResourceUri,
+					AuthenticationParameters.ApplicationId,
+					AuthenticationParameters.ReturnUri);
+
+				var userInfo = authenticationResult.UserInfo;
+
+				var userName = $"{userInfo.GivenName} {userInfo.FamilyName}";
+				await DisplayAlert("Success", userName, "Ok");
+			}
+			catch
+			{
+				await DisplayAlert("Failure", "Authentication failed.", "Ok");
+			}
 		}
 	}
 }
